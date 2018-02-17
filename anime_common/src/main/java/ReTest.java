bc926db7ceb8789e8ddcf215257c9fc71a922200
JlwkxeGi <Zkb496_16@163.com>
2018-02-18 00:13:04 +0800

    Initial commit

---
+++ anime_common/src/main/java/ReTest.java
@@ -0,0 +1,11 @@
+/**
+ * @program: onk_amine
+ * @description: test
+ * @author: chenh
+ * @create: 2018-02-17 20:01
+ **/
+public class ReTest {
+
+
+
+}
---
+++ anime_web_manage/src/main/java/com/onk/anime/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.onk.anime.controller;
+
+/**
+ * @program: onk_amine
+ * @description: base
+ * @author: chenh
+ * @create: 2018-02-17 23:44
+ **/
+public class BaseController {
+}
